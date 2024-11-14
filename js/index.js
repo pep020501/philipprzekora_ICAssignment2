f6590d21e70877114581aa2a8bcc8942bcce6dda
philip0205 <pep020501@gmail.com>
2024-11-14 16:53:42 -0500

    push

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function showA(){
+    document.getElementsById("image-containerA").style.visibility = 'visible';
+}
+
+function showB() {
+  document.getElementsByClassName("image-containerB").style.visibility = 'visible';
+}
+
+function showC() {
+  document.getElementsByClassName("image-containerC").style.visibility =
+    'visible';
+}
+
+function hideA() {
+  document.getElementsByClassName("image-containerA").style.visibility =
+    'hidden';
+}
+
+function hideB() {
+  document.getElementsByClassName("image-containerB").style.visibility =
+    'hidden';
+}
+
+function hideC() {
+  document.getElementsByClassName("image-containerC").style.visibility =
+    'hidden';
+}
+
